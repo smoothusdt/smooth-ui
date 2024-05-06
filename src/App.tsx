8b900d2c651d0644c3017c5473e6b1e430f6f31a
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-05-06 10:18:49 -0600

    Initial commit with MVP

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Smooth USDT</h1>
+      <div className="card">
+        <button>
+          New wallet
+        </button>
+        <button>
+          Import your wallet
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Smooth is a work in progress. <a href='https://info.smoothusdt.com/'>Learn more.</a>
+      </p>
+    </>
+  )
+}
+
+export default App
