3a8a1dfb1c3341b61b09ad35434049339ff36d77
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-05-15 14:25:34 -0600

    improve error handler

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -6,10 +6,12 @@ import { Button } from "@/components/ui/button";
 export const ErrorFallback = (props: FallbackProps) => {
   const { error, resetErrorBoundary } = props;
   return (
-    <Root>
-      <b>An error occurred:</b>
-      <PreWrap>{error.message}</PreWrap>
-      <Button onClick={resetErrorBoundary}>Refresh</Button>
+    <Root className="container flex flex-col justify-center size-full bg-background">
+      <div className="container flex flex-col justify-center max-w-[50ch] bg-background text-m gap-4">
+        <b>Something went wrong:</b>
+        <PreWrap>{error.message}</PreWrap>
+        <Button onClick={resetErrorBoundary}>Refresh</Button>
+      </div>
     </Root>
   );
 };
@@ -17,16 +19,18 @@ export const ErrorFallback = (props: FallbackProps) => {
 const PreWrap = styled.pre`
   overflow-wrap: break-word;
   white-space: pre-wrap;
-  max-width: 50ch;
 `;
 
 const Root = styled.div.attrs({ role: "alert" })`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-  background-color: pink;
-  font-size: 16px;
+  --dot-grid-color: hsl(var(--primary));
+  --dot-grid-bg-color: white;
+  --dot-grid-size: 32px;
+  --dot-grid-diameter: 1px;
+
+  background-size: var(--dot-grid-size) var(--dot-grid-size);
+  background-image: radial-gradient(
+    circle at calc(var(--dot-grid-size) / 2) calc(var(--dot-grid-size) / 2),
+    var(--dot-grid-color) var(--dot-grid-diameter),
+    var(--dot-grid-bg-color) 0
+  );
 `;
