88fefeffa726167b2ac872d235658aaee5c6b629
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-05-15 18:40:23 -0600

    Add theme switch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { ImportAccount } from "@/components/ImportAccount";
 import { useWallet } from "@/hooks/useWallet";
 import { Link } from "@/components/Link";
 import { Home } from "@/components/Home";
+import { ThemeSwitch } from "@/components/ThemeSwitch";
 
 function App() {
   const { connected } = useWallet();
@@ -9,11 +10,14 @@ function App() {
   return (
     <main className="container mx-auto w-96 flex flex-col justify-center">
       <div className="w-full h-full max-h-[900px] flex flex-col justify-between gap-4 py-8">
-        <div>
-          <h1 className="text-3xl font-semibold">Smooth USDT</h1>
-          <span className="text-sm text-muted-foreground">
-            Cheap, easy USDT TRC-20 payments
-          </span>
+        <div className="flex justify-between align-top">
+          <div>
+            <h1 className="text-3xl font-semibold">Smooth USDT</h1>
+            <span className="text-sm text-muted-foreground">
+              Cheap, easy USDT TRC-20 payments
+            </span>
+          </div>
+          <ThemeSwitch />
         </div>
 
         {connected ? <Home /> : <ImportAccount />}
--- src/main.tsx
+++ src/main.tsx
@@ -10,17 +10,21 @@ import { ErrorBoundary } from "react-error-boundary";
 import { WalletProvider } from "@/hooks/useWallet.tsx";
 import { TronWebProvider } from "@/hooks/useTronWeb.tsx";
 
+import { ThemeProvider } from "@/context/ThemeProvider";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
       onReset={() => window.location.reload()}
     >
-      <WalletProvider>
-        <TronWebProvider>
-          <App />
-        </TronWebProvider>
-      </WalletProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <WalletProvider>
+          <TronWebProvider>
+            <App />
+          </TronWebProvider>
+        </WalletProvider>
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>,
 );
