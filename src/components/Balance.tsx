5baa7d21c139474edd567f4f050aaf175821f7d9
Alexey Nebolsin <alexey.nebolsin.jr@gmail.com>
2024-05-22 19:17:23 +0500

    posthog when balance loads

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,8 +1,15 @@
 import { useUSDTBalance } from "@/hooks/useUSDTBalance";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect } from "react";
+import { usePostHog } from "posthog-js/react";
 
 export const Balance: React.FC = () => {
+  const posthog = usePostHog();
   const balance = useUSDTBalance();
+
+  useEffect(() => {
+    posthog.capture("Loaded user balance", { balance });
+  }, [balance, posthog]);
 
   return balance === undefined ? (
     <Skeleton className="w-36 h-9" />
