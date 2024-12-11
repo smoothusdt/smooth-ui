895c5cc10682f6016c1142f902a92082a1fba71f
Alexey Nebolsin <alexey.nebolsin.jr@gmail.com>
2024-12-11 21:43:21 +0400

    Show acitve pin cell

--- src/components/shared/EnterPin.tsx
+++ src/components/shared/EnterPin.tsx
@@ -2,10 +2,10 @@ import { AnimationControls, motion } from "framer-motion";
 import { DotIcon, Loader } from "lucide-react";
 import { useState } from "react";
 
-function DigitWindow(props: { filled: boolean; disabled?: boolean }) {
+function DigitWindow(props: { filled: boolean; focused: boolean; disabled?: boolean }) {
     return (
         <motion.div className="relative w-10 h-12 flex justify-center items-center">
-            <div className={`absolute w-full h-full border-current border-2 rounded-lg transition-all duration-300 ${props.disabled ? "opacity-5" : "opacity-15"}`}></div>
+            <div className={`absolute w-full h-full  border-2 rounded-lg transition-all duration-300 ${props.disabled ? "opacity-5" : "opacity-100"} ${props.focused && !props.disabled ? "border-gray-400" : "border-gray-700"}`}></div>
             <motion.div
                 initial={{ scale: 0 }}
                 animate={{ scale: props.filled ? 1 : 0 }}
@@ -58,7 +58,12 @@ export function EnterPin(props: {
                     className="absolute w-0"
                 />
                 {[...Array(props.pinLength).keys()].map(
-                    (value) => <DigitWindow disabled={props.processing || props.disabled} key={value} filled={pin.length >= value + 1} />
+                    (value) => <DigitWindow
+                        disabled={props.processing || props.disabled}
+                        key={value}
+                        focused={pin.length === value}
+                        filled={pin.length >= value + 1}
+                    />
                 )}
                 {props.processing && <div className="fixed w-full h-12 flex justify-center items-center">
                     <Loader className=" animate-spin w-8 h-8 opacity-70" />
